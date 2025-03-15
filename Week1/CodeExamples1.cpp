170e7aae1729d1e61766d452b9c3dc0b2b82bec1
Ankedefrey <ankedefrey@gmail.com>
2025-03-15 10:59:16 +0200

    Week 1: Seperation of concerns and basic structure of a c++ program

---
+++ Week1/CodeExamples1.cpp
@@ -0,0 +1,32 @@
+//Preprocessor Directives
+    //Directives for the preprocessor
+    //Interpreted before the compilation of the program itself.
+
+//It expands macros, includes header files and preprosses conditional compilation
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+
+int main() {
+    std::cout<<"Hello World!"<<std::endl;
+    return 0;
+}
+
+//Using namespace std
+    /*Allows for the organisation of code into logical groups and
+    //prevents name collision, especially when your code base includes
+    multiple libraries*/
+
+    /*Tells the compiler to use the standard namespace
+    std is the standard namespace that includes features such as I/O operations*/
+
+#include <iostream>
+
+using namespace std;
+
+/*Main function*/
+int main(){
+    cout<<"Hello World!">>endl;
+    return 0;
+}
